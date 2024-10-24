2cc6c24eaee21aa2ba9b11dfc3c721ad982ffa66
Danilo1dev <danilofoxlog@gmail.com>
2024-10-24 15:05:38 -0300

    init config

---
+++ src/test/java/com/mentory/spring/mock_pismo_service/MockPismoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentory.spring.mock_pismo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockPismoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
